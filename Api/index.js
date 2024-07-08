24cab3afdabe5f6b4841328b3828fc9e5f45a6ed
Marcos Damián Ezequiel Medina <marquitosmedina1@hotmail.com>
2024-07-08 10:37:36 -0300

    Base api project template

---
+++ Api/index.js
@@ -0,0 +1,14 @@
+import express from 'express'
+import cityWeather from './routes/cityWeather.js'
+
+const app = express();
+
+app.use('/weather', cityWeather);
+
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+  console.log(`Backend listening at http://localhost:${port}`);
+});
+
+export default app
