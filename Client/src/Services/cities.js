ad0b3c53fd620eed181b3c237981f298abde59c6
Marcos Damián Ezequiel Medina <marquitosmedina1@hotmail.com>
2024-07-11 21:50:03 -0300

    Production ready
    cityWeather fetching updated to receive the backend endpoint

--- Client/src/Services/cities.js
+++ Client/src/Services/cities.js
@@ -1,13 +1,11 @@
 import axios from 'axios';
-import dotenv from 'dotenv';
 
-dotenv.config();
-
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:3001';
+/* const LOCAL_URL = 'http://localhost:3001'; */
+const API_URL = 'https://climate-hub-api.vercel.app/'
 
 export const getCityWeather = async (city) => {
     try {
-        const { data } = await axios.get(`${API_URL}/api/weather/${city}`);
+        const { data } = await axios.get(`${API_URL}/weather/${city}`);
         return data;
     } catch (error) {
         console.error(`Error fetching weather data for ${city}:`, error);
