7263628318d3cc6d69e552e605e3bb878f9b0844
Marcos Damián Ezequiel Medina <marquitosmedina1@hotmail.com>
2024-07-12 16:12:10 -0300

    Axios frontend fetching updated to new backend deploy

--- Client/src/Services/cities.js
+++ Client/src/Services/cities.js
@@ -5,7 +5,7 @@ const API_URL = 'https://climate-hub-api.vercel.app'
 
 export const getCityWeather = async (city) => {
     try {
-        const { data } = await axios.get(`${API_URL}/weather/${city}`);
+        const { data } = await axios.get(`${API_URL}/${city}`);
         return data;
     } catch (error) {
         console.error(`Error fetching weather data for ${city}:`, error);
