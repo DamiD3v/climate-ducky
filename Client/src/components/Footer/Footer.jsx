d6f6d232cf2ee238cd675a402fb13560fde92adb
Marcos Damián Ezequiel Medina <marquitosmedina1@hotmail.com>
2024-07-12 21:15:45 -0300

    Update Footer.jsx

--- Client/src/components/Footer/Footer.jsx
+++ Client/src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import './Footer.css'
 const Footer = () => {
     return (
         <div className="footer-container">
-            <p>Climate Hub footer</p>
+            <p>Climate Duck</p>
         </div>
     )
 }
