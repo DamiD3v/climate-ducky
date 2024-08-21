cf206ab3b041ff66d3ac8822df269612b141a49f
Marcos Damián Ezequiel Medina <marquitosmedina1@hotmail.com>
2024-08-21 14:17:30 -0300

    Climate Ducky V1

--- Client/src/components/CityWeatherCardInfo/CityWeatherCardInfo.jsx
+++ Client/src/components/CityWeatherCardInfo/CityWeatherCardInfo.jsx
@@ -3,7 +3,7 @@ import { Element } from "react-scroll";
 import windFlag from '../../assets/windFlag.svg'
 import './CityWeatherCardInfo.css';
 
-const CityWeatherCardInfo = ({ data }) => {
+const CityWeatherCardInfo = ({ data, mobile }) => {
     if (!data) return (null);
 
     const { name, main, wind, weather } = data;
@@ -18,29 +18,57 @@ const CityWeatherCardInfo = ({ data }) => {
 
 
     return (
-        <div className="weather-card-container">
-            <div className="weather-card-name-container">
-                <Element name="mobile-cityName">
-                    <h2 className="weatherCityName">{name}</h2>
-                </Element>
-            </div>
-            <div className="weatherInfo-container">
-                <h3 className="weatherInfo-title">Temperature</h3>
-                <p className="weatherInfo">{temperatureCelsius}ºC</p>
-            </div>
-            <div className="weatherInfo-container">
-                <h3 className="weatherInfo-title">Weather Information</h3>
-                <p className="weatherInfo">{weatherMainInfo}</p>
-            </div>
-            <div className="weatherInfo-container-icon">
-                <h3 className="weatherInfo-title">Weather details</h3>
-                <p className="weatherInfo">{weatherDescription} <img src={`http://openweathermap.org/img/wn/${weatherIcon}@2x.png`} alt="Weather Icon" className="weatherInfo-icon" /></p>
-            </div>
-            <div className="weatherInfo-container">
-                <h3 className="weatherInfo-title">Wind Speed</h3>
-                <p className="weatherInfo">{windSpeedKmh} km/h <img className="windFlagIcon" src={windFlag} /></p>
-            </div>
-        </div >
+        <>
+            {mobile ? (
+                <div className="mobile-weather-card-container">
+                    <div className="mobile-weather-card-name-container">
+                        <Element name="mobile-cityName">
+                            <h2 className="mobile-weatherCityName">{name}</h2>
+                        </Element>
+                    </div>
+                    <div className="mobile-weatherInfo-container">
+                        <h3 className="mobile-weatherInfo-title">Temperature</h3>
+                        <p className="mobile-weatherInfo">{temperatureCelsius}ºC</p>
+                    </div>
+                    <div className="mobile-weatherInfo-container">
+                        <h3 className="mobile-weatherInfo-title">Weather Information</h3>
+                        <p className="mobile-weatherInfo">{weatherMainInfo}</p>
+                    </div>
+                    <div className="mobile-weatherInfo-container-icon">
+                        <h3 className="mobile-weatherInfo-title">Weather details</h3>
+                        <p className="mobile-weatherInfo">{weatherDescription} <img src={`http://openweathermap.org/img/wn/${weatherIcon}@2x.png`} alt="Weather Icon" className="weatherInfo-icon" /></p>
+                    </div>
+                    <div className="mobile-weatherInfo-container">
+                        <h3 className="mobile-weatherInfo-title">Wind Speed</h3>
+                        <p className="mobile-weatherInfo">{windSpeedKmh} km/h <img className="windFlagIcon" src={windFlag} /></p>
+                    </div>
+                </div >
+            ) : (
+                <div className="weather-card-container">
+                    <div className="weather-card-name-container">
+                        <Element name="cityName">
+                            <h2 className="weatherCityName">{name}</h2 >
+                        </Element >
+                    </div>
+                    <div className="weatherInfo-container">
+                        <h3 className="weatherInfo-title">Temperature</h3>
+                        <p className="weatherInfo">{temperatureCelsius}ºC</p>
+                    </div>
+                    <div className="weatherInfo-container">
+                        <h3 className="weatherInfo-title">Weather Information</h3>
+                        <p className="weatherInfo">{weatherMainInfo}</p>
+                    </div>
+                    <div className="weatherInfo-container-icon">
+                        <h3 className="weatherInfo-title">Weather details</h3>
+                        <p className="weatherInfo">{weatherDescription} <img src={`http://openweathermap.org/img/wn/${weatherIcon}@2x.png`} alt="Weather Icon" className="weatherInfo-icon" /></p>
+                    </div>
+                    <div className="weatherInfo-container">
+                        <h3 className="weatherInfo-title">Wind Speed</h3>
+                        <p className="weatherInfo">{windSpeedKmh} km/h <img className="windFlagIcon" src={windFlag} /></p>
+                    </div>
+                </div>
+            )}
+        </>
     );
 }
 
